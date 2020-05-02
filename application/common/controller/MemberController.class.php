5e3d53e6db81497050d42ee09873bf10451fd06d
yuwlong <1979556696@qq.com>
2020-05-02 15:51:32 +0800

    会员控制器基类

---
+++ application/common/controller/MemberController.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 会员控制器基类
+ */
+
+namespace common\controller;
+
+use think\Controller;
+
+class MemberController extends Controller
+{
+    protected function _initialize()
+    {
+    }
+
+    /**
+     * 空操作
+     */
+    public function _empty()
+    {
+    }
+}
