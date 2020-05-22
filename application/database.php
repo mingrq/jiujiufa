e88a5a8cc34d9da5e0ba500638f0e120d33a244b
ming <mingruqi@sina.cn>
2020-05-22 23:34:28 +0800

    添加短信 支付 sdk
    Signed-off-by: ming <mingruqi@sina.cn>

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => 'localhost',
     // 数据库名
     'database'        => 'sql_139760_com',
     // 用户名
-    'username'        => 'sql_139760_com',
+    'username'        => 'root',
     // 密码
-    'password'        => 'hWzsniwyyKTdzYGT',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
