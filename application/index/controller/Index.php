4af4658852d0da1f98cdc22c3d739c58318759ca
yuwlong <long10102>
2020-05-25 22:17:41 +0800

    首页

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,6 +6,7 @@
 namespace app\index\controller;
 
 use app\common\controller\FrontendBase;
+use app\common\model\Goods;
 
 class Index extends FrontendBase
 {
@@ -19,6 +20,11 @@ class Index extends FrontendBase
      */
     public function index()
     {
+        $goods = new Goods();
+        // 新品
+        $newGoodsList = $goods->where('good_state', '=', 1)->order('good_id', 'desc')->paginate(10);
+
+        $this->assign('newGoodsList', $newGoodsList);
         return $this->fetch();
     }
 }
