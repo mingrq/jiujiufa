b054691ec964ea1e29f2e55c36795f42dfa1a742
ming <mingruqi@sina.cn>
2020-05-30 21:46:39 +0800

    添加短信 支付 sdk
    Signed-off-by: ming <mingruqi@sina.cn>

--- application/admin/controller/Orderform.php
+++ application/admin/controller/Orderform.php
@@ -32,11 +32,44 @@ class Orderform extends AdminBaseController
     }
 
     /**
-     * 获取订单列表
+     * 搜索订单列表
      */
     public function searchorderlist()
     {
+        $condition = array();
 
+        $serach_param = input('param.searchparam');
+        $find = array('\\', '/', '%', '_', '&');
+        $replace = array('\\\\', '\\/', '\%', '\_', '\&');
+        $serach_param = '%' . trim(str_replace($find, $replace, $serach_param)) . '%';
+        if ($serach_param && trim($serach_param) != "") {
+            $condition['order_no|member_mobile|tracking_number|consignee_name'] = ['like', $serach_param];
+        }
+
+        $dateatart = input('param.dateatart');
+        $dateend = input('param.dateend');
+
+        if ($dateatart && $dateend) {
+            $condition["create_time"] = ['between', [$dateatart, $dateend]];
+        } else {
+            if ($dateatart) {
+                $condition["create_time"] = ['>=', $dateatart];
+            }
+
+            if ($dateend) {
+                $condition["create_time"] = ['<=', $dateend];
+            }
+        }
+
+        $query = db('v_order')
+            ->where($condition)
+            ->order('create_time desc')
+            ->paginate(100);
+        if ($query) {
+            ds_json_encode(10000, "搜索订单列表成功", $query);
+        } else {
+            ds_json_encode(10001, "搜索订单列表失败");
+        }
     }
 
 }
