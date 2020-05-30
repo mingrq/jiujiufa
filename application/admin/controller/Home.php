46d3b923c529a9eb180b268b439db288c24d0a28
ming <mingruqi@sina.cn>
2020-05-30 23:41:23 +0800

    添加短信 支付 sdk
    Signed-off-by: ming <mingruqi@sina.cn>

--- application/admin/controller/Home.php
+++ application/admin/controller/Home.php
@@ -12,6 +12,12 @@ namespace app\admin\controller;
 class Home extends AdminBaseController
 {
     public function home() {
+        $workordercount = db('workorder')->where('wo_state',1)->count();
+        $membercount =db('member')->count();
+        $warehousecount =db('warehouse')->count();
+        $this->assign('workordercount',$workordercount);
+        $this->assign('warehousecount',$warehousecount);
+        $this->assign('membercount',$membercount);
         return $this->fetch();
     }
 }
