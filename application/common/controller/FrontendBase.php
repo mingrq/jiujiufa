de26fbb1fbf5de368e82c1aaee655764b9237ce9
002 <mingruqi@sina.cn>
2020-06-03 17:52:15 +0800

    设置动态添加网站logo

--- application/common/controller/FrontendBase.php
+++ application/common/controller/FrontendBase.php
@@ -11,6 +11,28 @@ class FrontendBase extends Controller
 {
     protected function _initialize()
     {
+        $mode = model('websiteinfo');
+        $query = $mode->getWebsiteInfo();
+        foreach ($query as $info) {
+            if ($info['config_code'] == 'website_name') {
+                $this->assign('website_name', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_logo') {
+                $this->assign('website_logo', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_keyword') {
+                $this->assign('website_keyword', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_icp') {
+                $this->assign('website_icp', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_copyright') {
+                $this->assign('website_copyright', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_statistics') {
+                $this->assign('website_statistics', $info['config_value']);
+            }
+        }
     }
 
     /**
--- application/common/controller/MemberBase.php
+++ application/common/controller/MemberBase.php
@@ -14,6 +14,28 @@ class MemberBase extends Controller
      */
     protected function _initialize()
     {
+        $mode = model('websiteinfo');
+        $query = $mode->getWebsiteInfo();
+        foreach ($query as $info) {
+            if ($info['config_code'] == 'website_name') {
+                $this->assign('website_name', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_logo') {
+                $this->assign('website_logo', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_keyword') {
+                $this->assign('website_keyword', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_icp') {
+                $this->assign('website_icp', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_copyright') {
+                $this->assign('website_copyright', $info['config_value']);
+            }
+            if ($info['config_code'] == 'website_statistics') {
+                $this->assign('website_statistics', $info['config_value']);
+            }
+        }
         $MUserId = session('MUserId');
         $MUserName = session('MUserName');
         if (empty($MUserId) || empty($MUserName)) {
