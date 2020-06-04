63d5db9234c38a7fdfa6a8002560189752b1537b
002 <mingruqi@sina.cn>
2020-06-04 11:58:09 +0800

    调试个人中心

--- application/member/controller/Finance.php
+++ application/member/controller/Finance.php
@@ -32,16 +32,57 @@ class Finance extends MemberBase
     /**
      * 资金明细
      */
+
     public function cashRecord()
     {
-        
-        $moneychangeRecord = new MoneychangeRecord();
-        $recordList = $moneychangeRecord->where("member_id", '=', session('MUserId'))->order('change_time desc')->paginate(20);
-        $page = $recordList->render();
+        if (request()->isPost()) {
+            $condition = array();
+            $condition['member_id'] = session('MUserId');
+            $member_mode = model('member');
+            $query = $member_mode->getMoneychangeRecord($condition);
+            if ($query) {
+                ds_json_encode(10000, "获取资金明细成功", $query);
+            } else {
+                ds_json_encode(10001, "获取资金明细失败");
+            }
+        } else {
+            return $this->fetch();
+        }
+    }
 
-        $this->assign('recordList', $recordList);
-        $this->assign('page', $page);
+    /**
+     *搜索资金明细
+     */
+    public function serachcashrecord()
+    {
+        if (request()->isPost()) {
+            $condition = array();
+            $dateatart = input('post.dateatart');
+            $dateend = input('post.dateend');
 
-        return $this->fetch();
+            if ($dateatart && $dateend) {
+                $condition["change_time"] = ['between', [$dateatart, $dateend]];
+            } else {
+                if ($dateatart) {
+                    $condition["change_time"] = ['>=', $dateatart];
+                }
+
+                if ($dateend) {
+                    $condition["change_time"] = ['<=', $dateend];
+                }
+            }
+
+            $query = db('moneychange_record')
+                ->where($condition)
+                ->order('change_time desc')
+                ->paginate(100);
+            if ($query) {
+                ds_json_encode(10000, "搜索资金明细成功", $query);
+            } else {
+                ds_json_encode(10001, "搜索资金明细失败");
+            }
+        } else {
+            ds_json_encode(10001, "搜索资金明细失败");
+        }
     }
 }
