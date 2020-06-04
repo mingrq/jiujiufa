1c3a9370c060a2b744efeddff5a36db41eb09e91
ming <mingruqi@sina.cn>
2020-06-04 23:50:12 +0800

    调整ui
    Signed-off-by: ming <mingruqi@sina.cn>

---
+++ public/static/common/js/checkutils.js
@@ -0,0 +1,48 @@
+/**
+ * 验证手机号
+ * @param phone
+ * @returns {boolean}
+ */
+function checkPhone(phone){
+    if((/^1[3456789]\d{9}$/.test(phone))){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+
+/**
+ * 判断输入的是否是金额
+ * @param money
+ * @returns {boolean}
+ */
+function isMoney(money) {
+    var reg = /(^[1-9]([0-9]+)?(\.[0-9]{1,2})?$)|(^(0){1}$)|(^[0-9]\.[0-9]([0-9])?$)/;
+    if (reg.test(money)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//验证QQ号码5-11位
+function isQQ(qq) {
+    var filter = /^\s*[.0-9]{5,11}\s*$/;
+    if (!filter.test(qq)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//验证邮箱格式
+function isEmail(str) {
+    if (str.charAt(0) == "." || str.charAt(0) == "@" || str.indexOf('@', 0) == -1 ||
+        str.indexOf('.', 0) == -1 || str.lastIndexOf("@") == str.length - 1 ||
+        str.lastIndexOf(".") == str.length - 1 ||
+        str.indexOf('@.') > -1)
+        return false;
+    else
+        return true;
+}
