0be8a17565ac92e03f164a59747b5335ff066787
002 <mingruqi@sina.cn>
2020-06-05 18:10:51 +0800

    调试个人中心

--- application/member/controller/Payment.php
+++ application/member/controller/Payment.php
@@ -25,13 +25,17 @@ class Payment extends Controller
         $alipaymode = model('alipaymodel');
         $notify_result = $alipaymode->verify_notify();
         if ($notify_result) {
-            //db('recharge_record')->where('recharge_trade_no', 'CD31591349296247')->update(['trade_no' => 'sdfsdfsdfsd', 'test' => json_encode($notify_result),'recharge_state'=>2,'recharge_time'=>time()]);
+            if ($notify_result['trade_status'] == 1) {
+                //支付成功
+                $out_trade_no = $notify_result['out_trade_no']; #商户订单号
+                $trade_no = $notify_result['trade_no']; #交易凭据单号
+                $total_fee = $notify_result['total_fee']; #涉及金额
+                $order_type = $notify_result['order_type'];
+                db('recharge_record')->where('recharge_trade_no', $out_trade_no)->update(['trade_no' => $trade_no, 'trade_body' => $order_type, 'recharge_state' => 2, 'recharge_time' => time()]);
 
-            $out_trade_no = $notify_result['out_trade_no']; #商户订单号
-            $trade_no = $notify_result['trade_no']; #交易凭据单号
-            $total_fee = $notify_result['total_fee']; #涉及金额
-            $order_type = $notify_result['order_type'];
-            db('recharge_record')->where('recharge_trade_no', $out_trade_no)->update(['trade_no' => $trade_no, 'trade_body' => $order_type,'recharge_state'=>2,'recharge_time'=>time()]);
+            } else {
+                //支付失败
+            }
         }
     }
 
@@ -42,7 +46,12 @@ class Payment extends Controller
     {
         $alipaymode = model('alipaymodel');
         $notify_result = $alipaymode->return_verify();
-        $this->assign('tes',json_encode($notify_result));
-        return $this->fetch();
+        if ($notify_result['trade_status'] == 1) {
+            //支付成功
+            $this->assign('tes', json_encode($notify_result));
+            return $this->fetch();
+        } else {
+            //支付失败
+        }
     }
 }
