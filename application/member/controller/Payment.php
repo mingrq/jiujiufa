b8014c17b1c35e0202bef22a951e3a35dd5a1a8c
ming <mingruqi@sina.cn>
2020-06-05 22:21:04 +0800

    添加短信 支付 sdk
    Signed-off-by: ming <mingruqi@sina.cn>

--- application/member/controller/Payment.php
+++ application/member/controller/Payment.php
@@ -31,10 +31,13 @@ class Payment extends Controller
                 $total_fee = $notify_result['total_fee']; #涉及金额
                 $order_type = $notify_result['order_type'];
                 $time = date('Y-m-d H:i:s', time());
-                db('recharge_record')->where('recharge_trade_no', $out_trade_no)->update(['trade_no' => $trade_no, 'trade_body' => $order_type, 'recharge_state' => 2, 'recharge_time' => $time]);
-            } else {
-                //支付失败
-
+                $query = db('recharge_record')->where('recharge_trade_no', $out_trade_no)->update(['trade_no' => $trade_no, 'trade_body' => $order_type, 'recharge_state' => 2, 'recharge_time' => $time]);
+                if ($query) {
+                    //获取用户信息，添加用户余额，记录资金明细
+                    $memberid = db('recharge_record')->where('recharge_trade_no', $out_trade_no)->value('recharge_member_id');
+                    db('member')->where('member_id', $memberid)->setInc('member_balance', $total_fee);
+                    db('moneychange_record')->insert(['member_id' => $memberid, 'change_money' => $total_fee, 'change_cause' => $order_type]);
+                }
             }
         }
     }
